5e7ea6a8c03fbd152159f1b64d65c47e9bb59a78
henriB <henri.bretseau@gmail.com>
2021-09-22 10:39:56 +0200

    structure app

---
+++ src/components/App/index.js
@@ -0,0 +1,16 @@
+import "../../App.css";
+import Footer from "../Footer";
+import Header from "../Header";
+import Landing from "../Landing";
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <Landing></Landing>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer-container">
+        <p>Projet réaliser par DonkeyGeek - 2020</p>
+        <p>Sources des icônes : iconfinder.com </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
