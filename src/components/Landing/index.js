e15cceb51754578b407ba1f29b9e96c1fad61ffb
henriB <henri.bretseau@gmail.com>
2021-09-22 15:07:52 +0200

    animation wolferine

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useRef, useEffect, useState, Fragment } from "react";
 
 const Landing = () => {
+  const [btn, setBtn] = useState(false);
+  console.log("🚀 ~ file: index.js ~ line 5 ~ Landing ~ btn", btn);
+  const refWolferine = useRef(null);
+
+  useEffect(() => {
+    refWolferine.current.classList.add("startingImg");
+    setTimeout(() => {
+      refWolferine.current.classList.remove("startingImg");
+      setBtn(true);
+    }, 1000);
+  }, []);
+
+  const displayBtn = btn && (
+    <Fragment>
+      <div className="leftBox">
+        <button className="btn-welcome">Inscription</button>
+      </div>
+      <div className="rightBox">
+        <button className="btn-welcome">Connexion</button>
+      </div>
+    </Fragment>
+  );
   return (
-    <main className="welcomePage">
+    <main ref={refWolferine} className="welcomePage">
+      {displayBtn}
     </main>
   );
 };
