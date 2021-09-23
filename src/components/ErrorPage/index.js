dd5fabd7778276be7ab6265d339ddd57e995bd13
henriB <henri.bretseau@gmail.com>
2021-09-23 17:23:33 +0200

    design pageError

--- src/components/ErrorPage/index.js
+++ src/components/ErrorPage/index.js
@@ -1,9 +1,22 @@
 import React from "react";
+import batboom from "../../images/batman.png";
+
+const centerH2 = {
+  textAlign: "center",
+  marginTop: "50px",
+};
+const centerImg = {
+  display: "block",
+  margin: "40px auto",
+};
 
 const ErrorPage = () => {
   return (
     <div className="quiz-bg">
-      <div className="container">Erreur</div>
+      <div className="container">
+        <h2 style={centerH2}>Ooops, cette page n'existe pas !</h2>
+        <img style={centerImg} src={batboom} alt="error page" />
+      </div>
     </div>
   );
 };
