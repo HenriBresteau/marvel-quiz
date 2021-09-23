d4ed2013d08a50e2df3d9c626a5631364dc6f933
henriB <henri.bretseau@gmail.com>
2021-09-23 12:51:05 +0200

    structure router

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,11 +2,19 @@ import "../../App.css";
 import Footer from "../Footer";
 import Header from "../Header";
 import Landing from "../Landing";
+import Welcome from "../Welcome";
+import Login from "../Login";
+import Signup from "../SignUp";
+import ErrorPage from "../ErrorPage";
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <Welcome></Welcome>
+      <Login></Login>
+      <Signup></Signup>
+      <ErrorPage></ErrorPage>
       <Landing></Landing>
       <Footer></Footer>
     </div>
---
+++ src/components/Welcome/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Welcome = () => {
+  return (
+    <div className="quiz-bg">
+      <div className="container">
+        <h2>Welcome</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Welcome;
