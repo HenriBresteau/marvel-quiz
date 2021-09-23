f6f132d9e74cc42c649b32f373e5c14bcf39589a
henriB <henri.bretseau@gmail.com>
2021-09-23 13:18:14 +0200

    switch path

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -6,18 +6,23 @@ import Welcome from "../Welcome";
 import Login from "../Login";
 import Signup from "../SignUp";
 import ErrorPage from "../ErrorPage";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <Router>
       <Header></Header>
-      <Welcome></Welcome>
-      <Login></Login>
-      <Signup></Signup>
-      <ErrorPage></ErrorPage>
-      <Landing></Landing>
+
+      <Switch>
+        <Route path="/" exact component={Landing}></Route>
+        <Route path="/welcome" exact component={Welcome}></Route>
+        <Route path="/login" exact component={Login}></Route>
+        <Route path="/signup" exact component={Signup}></Route>
+        <Route component={ErrorPage}></Route>
+      </Switch>
+
       <Footer></Footer>
-    </div>
+    </Router>
   );
 }
 
