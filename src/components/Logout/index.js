cc35b0f31baddbfd626a8ea4bf3e28aa43f493c0
henriB <henri.bretseau@gmail.com>
2021-09-26 20:10:00 +0200

    Logout comp

---
+++ src/components/Logout/index.js
@@ -0,0 +1,27 @@
+import React, { useContext, useEffect, useState } from "react";
+import { FirebaseContext } from "../Firebase";
+const Logout = () => {
+  const [checked, setChecked] = useState(false);
+  const firebase = useContext(FirebaseContext);
+  console.log(checked);
+  useEffect(() => {
+    if (checked) {
+      console.log("déconnexion");
+      firebase.signoutUser();
+    }
+  }, [checked, firebase]);
+
+  const handleChange = (e) => {
+    setChecked(e.target.checked);
+  };
+  return (
+    <div className="logoutContainer">
+      <label className="switch">
+        <input type="checkbox" checked={checked} onChange={handleChange} />
+        <span className="slider round"></span>
+      </label>
+    </div>
+  );
+};
+
+export default Logout;
