0dd6d6fbece18134ac198d7ea5efedc9cdc94fff
henriB <henri.bretseau@gmail.com>
2021-10-05 19:06:58 +0200

    Question & options

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -1,20 +1,57 @@
 import ProgressBar from "../ProgressBar";
 import React, { Component } from "react";
 import Levels from "../Levels";
+import { QuizMarvel } from "../quizMarvel";
 
 class Quiz extends Component {
+  state = {
+    levelNames: ["debutant", "confirme", "expert"],
+    quizLevel: 0,
+    maxQuestions: 10,
+    storedQuestions: [],
+    question: null,
+    options: [],
+    idQuestion: 0,
+  };
+
+  loadQuestions = (level) => {
+    const feacthArrayQuiz = QuizMarvel[0].quizz[level];
+    if (feacthArrayQuiz.length >= this.state.maxQuestions) {
+      const newArray = feacthArrayQuiz.map(
+        ({ answer, ...keepRest }) => keepRest
+      );
+      this.setState({ storedQuestions: newArray });
+    } else {
+      console.log("pas assez de questions");
+    }
+  };
+  componentDidMount() {
+    this.loadQuestions(this.state.levelNames[this.state.quizLevel]);
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.storedQuestions !== prevState.storedQuestions) {
+      this.setState({
+        question: this.state.storedQuestions[this.state.idQuestion].question,
+        options: this.state.storedQuestions[this.state.idQuestion].options,
+      });
+    }
+  }
   render() {
     const { pseudo } = this.props.userData;
+    const displayOption = this.state.options.map((option, index) => {
+      return (
+        <p key={index} className="answerOptions">
+          {option}
+        </p>
+      );
+    });
     return (
       <div>
         <h2>Bonjour {pseudo} </h2>
         <Levels></Levels>
         <ProgressBar></ProgressBar>
-        <h2>Notre question Quiz</h2>
-        <p className="answerOptions">Question 1</p>
-        <p className="answerOptions">Question 2</p>
-        <p className="answerOptions">Question 3</p>
-        <p className="answerOptions">Question 4</p>
+        <h2> {this.state.question} </h2>
+        {displayOption}
         <button className="btnSubmit">Suivant</button>
       </div>
     );
