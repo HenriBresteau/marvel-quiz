f332af8ecaaf0f9a0eacadab17929170bef1cd31
henriB <henri.bretseau@gmail.com>
2021-10-18 11:00:51 +0200

    Props + table questions/answer

--- src/components/QuizOver/index.js
+++ src/components/QuizOver/index.js
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Fragment } from "react/cjs/react.production.min";
+const QuizOver = React.forwardRef((props, ref) => {
+  const [asked, setAsked] = useState([]);
 
-const QuizOver = () => {
-  return <div>QuizOver</div>;
-};
+  useEffect(() => {
+    setAsked(ref.current);
+  }, [ref]);
+  const questionAnswer = asked.map((question) => {
+    return (
+      <tr key={question.id}>
+        <td> {question.question} </td>
+        <td> {question.answer} </td>
+        <td>
+          <button className="btnInfo">Infos</button>
+        </td>
+      </tr>
+    );
+  });
+  return (
+    <Fragment>
+      <div className="stepsBtnContainer">
+        <p className="successMsg">Bravo, vous êtes un expert</p>
+        <button className="btnResult success">Niveau Suivant</button>
+      </div>
+      <div className="percentage">
+        <div className="progressPercent">Réussite : 10%</div>
+        <div className="progressPercent">Note : 10/10</div>
+      </div>
+      <hr />
+      <p>Les réponses aux questions posées: </p>
+      <div className="answerContainer">
+        <table className="answers">
+          <thead>
+            <tr>
+              <th>Question</th>
+              <th>Réponses</th>
+              <th>Infos</th>
+            </tr>
+          </thead>
+          <tbody>{questionAnswer}</tbody>
+        </table>
+      </div>
+    </Fragment>
+  );
+});
 
-export default QuizOver;
+export default React.memo(QuizOver);
