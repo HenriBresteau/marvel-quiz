76bb6d36a8e853c95daa58fdfadaa6b2a728419e
henriB <henri.bretseau@gmail.com>
2021-10-18 18:06:51 +0200

    Props to QuizOver + Decision by score

--- src/components/QuizOver/index.js
+++ src/components/QuizOver/index.js
@@ -1,32 +1,73 @@
 import React, { useEffect, useState } from "react";
 import { Fragment } from "react/cjs/react.production.min";
 const QuizOver = React.forwardRef((props, ref) => {
+  const { levelNames, score, maxQuestions, quizLevel, percent } = props;
   const [asked, setAsked] = useState([]);
 
   useEffect(() => {
     setAsked(ref.current);
   }, [ref]);
-  const questionAnswer = asked.map((question) => {
-    return (
-      <tr key={question.id}>
-        <td> {question.question} </td>
-        <td> {question.answer} </td>
-        <td>
-          <button className="btnInfo">Infos</button>
+  const avgGrade = maxQuestions / 2;
+  const decision =
+    score >= avgGrade ? (
+      <Fragment>
+        <div className="stepsBtnContainer">
+          {quizLevel < levelNames.length ? (
+            <Fragment>
+              <p className="successMsg">Bravo, passez au niveau suivant !</p>
+              <button className="btnResult success">Niveau Suivant</button>
+            </Fragment>
+          ) : (
+            <Fragment>
+              <p className="successMsg">Bravo, vous êtes un expert !</p>
+              <button className="btnResult gameOver">Niveau Suivant</button>
+            </Fragment>
+          )}
+        </div>
+        <div className="percentage">
+          <div className="progressPercent">Réussite : {percent}%</div>
+          <div className="progressPercent">
+            Note : {score}/{maxQuestions}
+          </div>
+        </div>
+      </Fragment>
+    ) : (
+      <Fragment>
+        <div className="stepsBtnContainer">
+          <p className="failureMsg">Vous avez échoué !</p>
+        </div>
+        <div className="percentage">
+          <div className="progressPercent">Réussite : {percent}%</div>
+          <div className="progressPercent">
+            Note : {score}/{maxQuestions}
+          </div>
+        </div>
+      </Fragment>
+    );
+
+  const questionAnswer =
+    score >= avgGrade ? (
+      asked.map((question) => {
+        return (
+          <tr key={question.id}>
+            <td> {question.question} </td>
+            <td> {question.answer} </td>
+            <td>
+              <button className="btnInfo">Infos</button>
+            </td>
+          </tr>
+        );
+      })
+    ) : (
+      <tr>
+        <td colSpan="3">
+          <p style={{ textAlign: "center", color: "red" }}>Pas de réponses !</p>{" "}
         </td>
       </tr>
     );
-  });
   return (
     <Fragment>
-      <div className="stepsBtnContainer">
-        <p className="successMsg">Bravo, vous êtes un expert</p>
-        <button className="btnResult success">Niveau Suivant</button>
-      </div>
-      <div className="percentage">
-        <div className="progressPercent">Réussite : 10%</div>
-        <div className="progressPercent">Note : 10/10</div>
-      </div>
+      {decision}
       <hr />
       <p>Les réponses aux questions posées: </p>
       <div className="answerContainer">
