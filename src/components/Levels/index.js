baa75d3487b3e5df0b437fd23e11d47440d35d0d
henriB <henri.bretseau@gmail.com>
2021-10-19 19:20:11 +0200

    Props + Stepper

--- src/components/Levels/index.js
+++ src/components/Levels/index.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Stepper from "react-stepper-horizontal";
+const Levels = ({ levelNames, quizLevel }) => {
+  const [levels, setLevels] = useState([]);
 
-const Levels = () => {
+  useEffect(() => {
+    const quizSteps = levelNames.map((lvl) => ({ title: lvl.toUpperCase() }));
+    setLevels(quizSteps);
+  }, [levelNames]);
   return (
-    <div className="levelsContainer">
-      <h2 className="headingLevels">Débutant</h2>
+    <div className="levelsContainer" style={{ background: "transparent" }}>
+      <Stepper
+        steps={levels}
+        activeStep={quizLevel}
+        circleTop={0}
+        activeTitleColor={"#d31017"}
+        activeColor={"#eb1d27"}
+        completeTitleColor={"#e0e0e0"}
+        completeColor={"#e0e0e0"}
+        completeBarColor={"#e0e0e0"}
+        defaultTitleColor={"#e0e0e0"}
+        barStyle={"dashed"}
+        size={46}
+        circleFontSize={20}
+      />
     </div>
   );
 };
 
-export default Levels;
+export default React.memo(Levels);
